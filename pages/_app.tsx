a57413895e02e3473b9bdf3a8188645075f504e2
Diego Carrillo <61395884+DiegoCarrillo32@users.noreply.github.com>
2022-11-05 23:14:14 -0600

    revers changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,14 +10,14 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Diego Carrillo Blog</title>
+        <title>Diego Carrillo</title>
         <meta charSet="UTF-8"/>
-        <meta name="keywords" content="Frontend, developer, backend, javascript, node, blog, development,"/>
+        <meta name="keywords" content="Frontend, developer, backend, javascript, node"/>
         <meta name="author" content="Diego Carrillo"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="I am a Computer engineer that focues on web solutions!"/>
+        <meta name="description" content="I am a Computer engineer, focues on web solutions!"/>
         <link rel="icon" href="./pixil-frame-0.png" />
         <link rel="apple-touch-icon" href="./logo192.png" />
         
